6a8ae1c59ca4c17c4f99a43e5bf73e52025a6e74
Vol.d'Marius <115695164+VoldMarius@users.noreply.github.com>
2023-06-07 10:49:17 +0300

    Initial commit

--- 1.py
+++
@@ -1,7 +0,0 @@
-import pandas as pd
-
-# create DataFrame
-df = pd.DataFrame({'team': ['D', 'D', 'E', 'E'],
-                   'points': [6, 4, 4, 7],
-                   'rebounds': [15, 18, 9, 12]})
-print(df)
